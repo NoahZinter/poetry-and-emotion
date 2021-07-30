07d6f6f167a2d9b9c96ffb9779c594943c3d80ac
NoahZinter <noah.zinter@gmail.com>
2021-07-30 11:37:51 -0600

    Add tones to poem poro

--- app/poros/poem.rb
+++ app/poros/poem.rb
@@ -1,11 +1,13 @@
 class Poem
   attr_reader  :author,
                :lines,
-               :title
+               :title,
+               :tones
 
   def initialize(data)
     @author = data[:author]
     @lines = data[:lines]
+    @tones = IBMToneService.tones(@lines.join)[:document_tone][:tones]
     @title = data[:title]
   end
 end
--- spec/poros/poem_spec.rb
+++ spec/poros/poem_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe Poem do
     expect(poem.author).to eq "Emily Dickinson"
     expect(poem.title).to eq "Not at Home to Callers"
     expect(poem.lines).to eq(["Not at Home to Callers", "Says the Naked Tree --", "Bonnet due in April --", "Wishing you Good Day --"])
+    expect(poem.tones).to eq([{:score=>0.773473, :tone_id=>"joy", :tone_name=>"Joy"}])
   end
 end
