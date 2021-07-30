4405e259505882237f97113b0b0e68bb64663104
NoahZinter <noah.zinter@gmail.com>
2021-07-30 11:00:02 -0600

    Try and account for edge cases

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
   def index
     @author = params[:author]
+    if PoemFacade.search(@author).present?
+      @poems = PoemFacade.search(@author)
+    else
+      @poems = "No results found"
+    end
   end
 end
--- app/facades/poem_facade.rb
+++ app/facades/poem_facade.rb
@@ -1,8 +1,14 @@
 class PoemFacade
   def self.search(author)
     data = PoetryDBService.author_search(author)
-    data[0..9].map do |data|
-      Poem.new(data)
+    if data.length >= 10
+      data[0..9].map do |data|
+        Poem.new(data)
+      end
+    else
+      data.map do |data|
+        Poem.new(data)
+      end
     end
   end
 end
