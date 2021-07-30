6677a1d9bf872c71fc48f0d62639367cca16c2c4
NoahZinter <noah.zinter@gmail.com>
2021-07-30 11:25:10 -0600

    Test ibm service response

--- spec/services/ibm_tone_service_spec.rb
+++ spec/services/ibm_tone_service_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe IBMToneService do
   it 'analyzes tones' do
     text = 'Not at Home to Callers Says the Naked Tree -- Bonnet due in April -- Wishing you Good Day --'
     data = IBMToneService.tones(text)
-    binding.pry
+
+    expect(data[:document_tone][:tones]).is_a? Array
+    data[:document_tone][:tones].each do |tone|
+      expect(tone[:tone_name]).is_a? String
+    end
   end
 end
